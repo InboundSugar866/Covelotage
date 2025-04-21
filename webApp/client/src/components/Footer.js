694606953d6dd3dbd70fd87633a393d8ef495b0f
InboundSugar866 <146993818+InboundSugar866@users.noreply.github.com>
2025-04-21 11:18:18 +0200

    Bugfix
    Style du Footer ok sur écrans 16:9.

--- webApp/client/src/components/Footer.js
+++ webApp/client/src/components/Footer.js
@@ -20,32 +20,64 @@ import { ReactComponent as Route } from '../assets/Route.svg';
 // Html
 const Footer = () => {
   return (
-    <footer class="footer text-black text-center d-flex flex-column align-items-center" style={{height: "25vh", fontweight: "bold", fontsize: "1.3rem", backgroundColor: '#588157'}}>
-      <div class="w-100">
-        <Route class="w-100" />
+    <footer
+      className="footer text-black text-center d-flex flex-column align-items-center"
+      style={{
+        minHeight: "25vh", // Ensure the footer occupies at least 25% of the viewport height
+        fontWeight: "bold",
+        fontSize: "1.3rem",
+        backgroundColor: "#588157",
+      }}
+    >
+      <div className="w-100">
+        <Route className="w-100" />
       </div>
 
-      <div class="d-flex flex-column justify-content-center align-items-center w-100 py-2" style={{flex: "1"}}>
-        <div class="d-flex justify-content-between w-100 mb-2">
-          <div class="d-flex flex-column align-items-center justify-content-center me-5" style={{flex: "1"}}>
+      <div
+        className="d-flex flex-column justify-content-center align-items-center w-100 py-2"
+        style={{ flex: "1" }}
+      >
+        <div className="d-flex flex-wrap justify-content-between w-100 mb-2">
+          <div
+            className="d-flex flex-column align-items-center justify-content-center me-5"
+            style={{ flex: "1", minWidth: "10vw" }}
+          >
             <h3>COVELOTAGE</h3>
             <Link to="/login">
-              <YourSvg class="w-100" style={{width: "300px", height: "100px"}}/>
+              <YourSvg
+                className="w-100"
+                style={{ maxWidth: "15vw", height: "auto" }}
+              />
             </Link>
           </div>
 
-          <div class="d-flex flex-column align-items-center justify-content-center" style={{flex: "1"}}>
-            <h3>UNE INITIATIVE DU PDIE <br/> TECHNOPOLE NANCY BRABOIS</h3>
-            <a href="https://pdiebrabois.wordpress.com/">
-              <img src={logoPdie} target='_blank' alt='logo_pdie' class="w-100" style={{width: "400px", height: "100px"}}/>
+          <div
+            className="d-flex flex-column align-items-center justify-content-center"
+            style={{ flex: "1", minWidth: "10vw" }}
+          >
+            <h3>
+              UNE INITIATIVE DU PDIE <br /> TECHNOPOLE NANCY BRABOIS
+            </h3>
+            <a href="https://pdiebrabois.wordpress.com/" target="_blank" rel="noopener noreferrer">
+              <img
+                src={logoPdie}
+                alt="logo_pdie"
+                className="w-100"
+                style={{ maxWidth: "15vw", height: "auto" }}
+              />
             </a>
           </div>
         </div>
 
-        <span>© {new Date().getFullYear()} Covélotage - PDIE Technopôle Nancy-Brabois - <Link to="/Mentions" class="text-black">Mentions légales</Link></span>
+        <span>
+          © {new Date().getFullYear()} Covélotage - PDIE Technopôle Nancy-Brabois -{" "}
+          <Link to="/Mentions" className="text-black">
+            Mentions légales
+          </Link>
+        </span>
       </div>
     </footer>
-    );
-  };
+  );
+};
   
   export default Footer;
